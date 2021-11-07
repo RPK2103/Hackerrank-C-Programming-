09202059142664cc2968944c0036f119ad74c0cf
Kaviyashre Ragupathy <60558428+RPK2103@users.noreply.github.com>
2021-11-07 23:43:39 +0530

    Update Conditional Statements.c

--- Conditional Statements.c
+++ Conditional Statements.c
@@ -14,7 +14,6 @@ int main()
     char* n_endptr;
     char* n_str = readline();
     int n = strtol(n_str, &n_endptr, 10);
-
     if (n_endptr == n_str || *n_endptr != '\0') { exit(EXIT_FAILURE); }
     if(1<=n && n<=9)
         {
